a3f414b912fd7e694c7ad2fe5334568c73e62629
korilium <73854608+korilium@users.noreply.github.com>
2023-03-15 22:21:21 +0100

    first update on library

---
+++ src/extract.jl
@@ -0,0 +1,16 @@
+function fin_data(Tickers,days = 250, clientKey = "0VS2G38H6PKP03GX" )
+
+    #extract the data 
+    client = AlphaVantage.GLOBAL[]
+    client.key = clientKey
+    AlphaVantage.GLOBAL[]
+    portfolio = []
+    days= days
+    for i in Tickers 
+        asset = DataFrame(time_series_daily_adjusted( i, outputsize= "full"))
+        asset = asset[1:days,:]
+        asset[!,"ticker"] .= i 
+        push!(portfolio, asset)
+    end 
+    return portfolio
+end 
