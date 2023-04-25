0216007a2f34da0ac11b96db22534ccaca5de123
korilium <ignace.decocq@hotmail.com>
2023-04-25 22:49:59 +0200

    update modern market portfolio

--- examples/example_mpt.jl
+++ examples/example_mpt.jl
@@ -1,24 +1,24 @@
-using Peccon, StatsPlots, AlphaVantage, DataFrames, Statistics
+using Peccon, StatsPlots
 
-Tickers = ["VOO", "BSV", "GLD"]
+Tickers = ["IUSA.AS", "IBCI.AS", "IEMA.AS", "WTCH.AS", "VWRL.AS"]
 
-data1 = fin_data(Tickers, "0VS2G38H6PKP03GX", 240)
+
+data1 = data_alpha(Tickers, "0VS2G38H6PKP03GX", 248)
 
 returns = daily_returns(data1, Tickers)
+
+
 port_sim = sim_mpt(returns,5000)
 @df port_sim scatter(:port_var, :exp_return)
 
 # port_sharp = sharp_ratio(port_sim)
 
-port_opt =  opt_mpt(returns, 0.0:0.02:2.0, 0.00 )
+port_opt =  opt_mpt(returns, 0.0:0.02:2.0, 0.00)
+
 
 @df port_opt scatter!(:port_var, :exp_return)
 
 
-sharp = sharp_ratio(port_sim)
 
 
-all(sharp[end,:exp_return] .>  sharp[sharp[end,:port_std] .> sharp[:,:port_std], :exp_return])
 
-
-df = time_series_daily_adjusted( "VWRL", outputsize= "full")
