5f71a43c406f76f9492c9f80d5074f1523ab2581
korilium <ignace.decocq@hotmail.com>
2023-06-26 00:39:17 +0200

    fitting pieces together for PMPT

--- examples/deep_learning.jl
+++ examples/deep_learning.jl
@@ -116,4 +116,77 @@ model3 = Chain( Dense(10 => 5 , σ),
                 Dense(5 => 2), 
                 softmax)
 
-                
+  
+x = [1,2,3]
+
+x = [1 2 ; 3 4 ]
+
+x = rand(5,3)
+
+x = rand(BigFloat, 5,3 )
+
+length(x)
+
+size(x)
+
+x[2,3]
+
+
+x .+ 1 
+
+zeros(5,5) .+ (1:5)'
+
+(1:5) .* (1:5)
+
+
+f(x) = 3x^2 + 2x +1
+
+using Flux: gradient
+
+df(x) = gradient(f, x)[1]
+
+
+using Flux: params 
+
+W = randn(3,5)
+b = zeros(3)
+x = rand(5)
+
+
+y(x) = sum(W* x .+ b)
+
+
+grads = gradient(() -> y(x), params([W,b]))
+
+
+grads[W]
+
+using Flux 
+
+m = Dense(10,5)
+
+x = rand(Float32,10)
+
+params(m)
+
+x = rand(Float32, 10)
+m = Chain(Dense(10,5,relu), Dense(5,2), softmax)
+l(x) = sum(Flux.crossentropy(m(x), [0.5,0.5]))
+grads = gradient(params(m)) do 
+  l(x)
+end
+
+for p in params(m)
+  println(grads[p])
+end 
+
+opt = Descent(0.01)
+
+data, labels = rand(10,100), fill(0.5,2,100)
+loss(x, y) = sum(Flux.crossentropy(m(x), y))
+Flux.train!(loss, params(m),[(data, labels)], opt )
+
+fill(0.5,2,100)
+
+
+params(m)
--- examples/example_nmpt.jl
+++ examples/example_nmpt.jl
@@ -61,6 +61,8 @@ model_test = Chain( Dense(size(X)[1] => 42 , σ),
                 softmax)
 
 
+
+
 returns = daily_returns(data, Tickers)
 
 
@@ -74,3 +76,56 @@ loss(0.000000000000988, stock_price2, 1008)
 
 loss(0.01, stock_price3, 1008)
 
+
+# apply flux for neural micture models 
+
+#  create data  
+# Generate data from two Gaussian distributions
+μ1, σ1 = 0.0, 0.5
+μ2, σ2 = 3.0, 1.0
+data = [rand(Normal(μ1, σ1)) for _ in 1:1000] .+ rand(Normal(μ2, σ2), 1000)
+
+
+# Define the deep neural mixture model
+function mixture_model(input_dim, num_components)
+    return Chain(
+        Dense(input_dim, 64, relu),
+        Dense(64, 64, relu),
+        Dense(64, num_components * 3),  # Each mixture component has 3 parameters (mean, std, weight)
+    )
+end
+
+num_components = 2  # Number of mixture components
+model = mixture_model(1, num_components)
+
+
+# loss function 
+
+function negative_log_likelihood(model, data)
+    mixture_params = model(data)
+
+    # Extract mixture parameters
+    means = reshape(mixture_params[:, 1:num_components], :, num_components)
+    stds = reshape(exp.(mixture_params[:, num_components+1:2*num_components]), :, num_components)
+    weights = softmax(mixture_params[:, 2*num_components+1:end], dims=2)
+
+    # Compute negative log-likelihood
+    likelihoods = pdf.(Normal.(means, stds), data')
+    weighted_likelihoods = likelihoods .* weights
+    log_likelihoods = log.(sum(weighted_likelihoods, dims=2))
+    return -mean(log_likelihoods)
+end
+
+
+# optimize 
+
+using Flux.Optimise
+
+# Define optimizer and hyperparameters
+optimizer = ADAM(0.01)
+num_epochs = 100
+
+# Training loop
+for epoch in 1:num_epochs
+    Flux.train!(negative_log_likelihood, Flux.params(model), [(data,)], optimizer)
+end
